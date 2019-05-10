ebc259479a6e757134b616ca6c4462e9f085d9f2
one-ccs <49385353+one-ccs@users.noreply.github.com>
2019-05-10 19:04:49 +0800

    Update main.py
    1.消除倒计时剩30秒时，再次在系统中倒计时输入时间的秒数
    2.解决输入非数字时闪退的bug

--- main.py
+++ main.py
@@ -40,6 +40,14 @@ def menu_display():
     + "6、退出\n"
     return view
 
+def enter_num(info = "请输入数字："):
+    num = 'a'
+    
+    while (not num.isdigit()):
+        num = input(info)
+    
+    return num
+
 def rev_nums(a):
     a = list(range(int(a), -1, -1))
     return a
@@ -50,22 +58,22 @@ def my_shutdown(command):
     import sys
     
     if command != 3:
-        time_down = input("请输入定时时间:")
+        time_down = enter_num("请输入定时时间(s):")
     
     if command == 0:
         cmd = '关机'
-        command = 'shutdown /s /t ' + str(time_down)
+        command = 'shutdown /s /t '
     elif command == 1:
         cmd = '重启'
-        command = 'shutdown /r /t ' + str(time_down)
+        command = 'shutdown /r /t '
     elif command == 2:
         cmd = '注销'
         command = 'shutdown /l /f'
     elif command == 3:
         os.system("shutdown /a")
-        input("已清除计划任务，按任意键继续...")
+        input("已清除计划任务，按回车键继续...")
         os.system("cls")
-        return 0;
+        return 0
     
     a = 0
     for i in rev_nums(time_down):
@@ -73,47 +81,55 @@ def my_shutdown(command):
             msvcrt.getch()
             return 0
             
-        sys.stdout.write("\r%ds 后%s，按任意键取消..." %(i, cmd))
+        #sys.stdout.write("\r%ds 后%s，按任意键取消..." %(i, cmd))
+        print("\r%ds 后%s，按任意键取消..." %(i, cmd), end = '')
         if int(i) == 30 and command != 'shutdown /l /f':
             a += 1
+            command = command + "30"
             os.system(command)
         elif int(i) < 30 and a == 0:
+            command = command + str(time_down)
             os.system(command)
             a += 1
         time.sleep(1)
 
-os.system('title AutoSd by one-ccs')
-os.system('color 0a')
-os.system('mode con: cols=40 lines=26')
+def main():
+    os.system('title AutoSd by one-ccs')
+    os.system('color 0a')
+    os.system('mode con: cols=40 lines=26')
 
-while 1:
-    print(tip_display())
-    print(real_time())
-    print(menu_display())
-    
-    choose = input("请输入数字：")
-    if int(choose) == 1:
-        my_shutdown(0)
-        input("\n按任意键继续...")
-        os.system("cls")
-    elif int(choose) == 2:
-        my_shutdown(1)
-        input("\n按任意键继续...")
-        os.system("cls")
-    elif int(choose) == 3:
-        my_shutdown(2)
-        input("\n按任意键继续...")
-        os.system("cls")
-    elif int(choose) == 4:
-        my_shutdown(3)
-        continue
-    elif int(choose) == 5:
-        os.system("cls")
-        continue
-    elif int(choose) == 6:
-        sys.exit(0)
-    else:
-        print("Error!")
-        input("按任意键继续")
-        os.system("cls")
-        continue
+    while 1:
+        print(tip_display())
+        print(real_time())
+        print(menu_display())
+
+        choose = enter_num()
+        
+        if int(choose) == 1:
+            my_shutdown(0)
+            input("\n按回车键继续...")
+            os.system("cls")
+        elif int(choose) == 2:
+            my_shutdown(1)
+            input("\n按回车键继续...")
+            os.system("cls")
+        elif int(choose) == 3:
+            my_shutdown(2)
+            input("\n按回车键继续...")
+            os.system("cls")
+        elif int(choose) == 4:
+            my_shutdown(3)
+            continue
+        elif int(choose) == 5:
+            os.system("cls")
+            continue
+        elif int(choose) == 6:
+            sys.exit(0)
+        else:
+            print("Error!")
+            input("按回车键继续")
+            os.system("cls")
+            continue
+
+if __name__ == "__main__":
+    main()
